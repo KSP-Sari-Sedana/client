28572eb1761cad15b60bb6be4ecb8dec7b2a30f6
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:49:57 +0800

    style: about component

---
+++ src/components/interface/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div id="about">
+      <img src="/assets/placeholder-product.svg" alt="About" className="mx-auto w-52" />
+      <p className="mt-5 text-center">
+        Melalui pelayanan <span className="font-bold">PRIMA</span> untuk peningkatan kesejahteraan anggota dan masyarakat
+      </p>
+    </div>
+  );
+}
+
+export { About };
