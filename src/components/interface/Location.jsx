6e5a41c3a1a63435b65638aeb6f6e447f2db8a8c
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:49:04 +0800

    style: location component

---
+++ src/components/interface/Location.jsx
@@ -0,0 +1,32 @@
+function Location() {
+  return (
+    <div>
+      <div className="grid grid-cols-3 mx-20">
+        <div>
+          <img src="/assets/placeholder-product.svg" alt="" className="mx-auto w-52" />
+          <p className="font-sourcecodepro text-xl font-bold text-center mt-3">KCP RENDANG</p>
+          <p className="text-center text-sm text-slate-600">
+            Jl. Gunung Guning No. 69, Singarata <br /> Rendang, Karangasem, Bali 80863
+          </p>
+        </div>
+        <div>
+          <img src="/assets/placeholder-product.svg" alt="" className="mx-auto w-60" />
+          <p className="font-sourcecodepro text-2xl font-bold text-center mt-3">KANTOR PUSAT SELAT</p>
+          <p className="text-center text-slate-600">
+            Pering Sari, Kec. Selat, Kabupaten <br /> Karangasem, Bali 80862
+          </p>
+        </div>
+        <div>
+          <img src="/assets/placeholder-product.svg" alt="" className="mx-auto w-52" />
+          <p className="font-sourcecodepro text-xl font-bold text-center mt-3">KCP KARANGASEM</p>
+          <p className="text-center text-sm text-slate-600">
+            Jl. Veteran, Padang Kerta, Karangasem, <br />
+            Bali 80811
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Location };
