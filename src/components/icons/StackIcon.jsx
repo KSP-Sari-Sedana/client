e91fa00cedc919178b67c23298c599734296480c
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:07:03 +0800

    style: stack icon

---
+++ src/components/icons/StackIcon.jsx
@@ -0,0 +1,28 @@
+function StackIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 20 21">
+      <path
+        fill="#28303F"
+        fillRule="evenodd"
+        d="M8.52.29a2.75 2.75 0 012.46 0l7.317 3.659c1.29.645 1.29 2.485 0 3.13l-7.317 3.659a2.75 2.75 0 01-2.46 0L1.204 7.079c-1.29-.645-1.29-2.485 0-3.13L8.52.29z"
+        clipRule="evenodd"
+      ></path>
+      <path
+        fill="#28303F"
+        fillRule="evenodd"
+        d="M.065 15.21a.75.75 0 01.99-.382l8.188 3.64a1.25 1.25 0 001.015 0l8.188-3.64a.75.75 0 11.609 1.371l-8.188 3.64a2.75 2.75 0 01-2.234 0l-8.187-3.64a.75.75 0 01-.381-.99z"
+        clipRule="evenodd"
+        opacity="0.4"
+      ></path>
+      <path
+        fill="#28303F"
+        fillRule="evenodd"
+        d="M.065 10.71a.75.75 0 01.99-.382l8.188 3.64a1.25 1.25 0 001.015 0l8.188-3.64a.75.75 0 11.609 1.371l-8.188 3.64a2.75 2.75 0 01-2.234 0l-8.187-3.64a.75.75 0 01-.381-.99z"
+        clipRule="evenodd"
+        opacity="0.4"
+      ></path>
+    </svg>
+  );
+}
+
+export { StackIcon };
