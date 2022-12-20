0315e2220c64521bff25bdf0e51d18bf6cefb99c
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:44:46 +0800

    style: intro component

---
+++ src/components/interface/Intro.jsx
@@ -0,0 +1,21 @@
+function Intro() {
+  return (
+    <div>
+      <div>
+        <div>
+          <p className="font-darkergrotesque text-center text-6xl font-bold mt-40">
+            <span className="tracking-tight">
+              Koperasi Simpan Pinjam <br /> dengan Pelayanan
+            </span>
+            <span className="font-black tracking-tight"> PRIMA.</span>
+          </p>
+        </div>
+        <div className="flex mx-60">
+          <img src="/assets/placeholder-product.svg" alt="" className="mx-auto mt-7 w-60" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Intro };
