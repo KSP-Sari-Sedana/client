04cb6c6cbeac337617924cee7284c2b195239e16
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:34:33 +0800

    style: user menu component

---
+++ src/components/interface/UserMenu.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import { Fragment } from "react";
+
+import { Avatar } from "./Avatar";
+import { GoldStarIcon } from "../icons/GoldStarIcon";
+import { DashboardIcon } from "../icons/DashboardIcon";
+import { ProfileCircleIcon } from "../icons/ProfileCircleIcon";
+import { DoorIcon } from "../icons/DoorIcon";
+import { useToggleContext } from "../../context/toggleContext";
+
+function UserMenu() {
+  const { isPopupAvatar, togglePopupAvatar } = useToggleContext();
+
+  return (
+    <Fragment>
+      <Avatar action={togglePopupAvatar} />
+      {isPopupAvatar && (
+        <div className="w-60 rounded-2xl bg-white border border-slate-200 shadow-sm px-3 py-3 absolute top-[67px] right-0">
+          <div className="flex items-center">
+            <Avatar />
+            <div className="ml-3">
+              <p className="font-darkergrotesque font-bold text-2xl leading-none">Suprapta</p>
+              <div className="flex items-center">
+                <GoldStarIcon />
+                <p className="text-sm text-slate-500 leading-tight">Admin</p>
+              </div>
+            </div>
+          </div>
+          <hr className="my-3 h-px bg-gray-200 border-0"></hr>
+          <Link to="/dashboard">
+            <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md" onClick={togglePopupAvatar}>
+              <DashboardIcon />
+              <span className="ml-2 text-sm">Dashboard</span>
+            </div>
+          </Link>
+          <Link to="/profile">
+            <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md" onClick={togglePopupAvatar}>
+              <ProfileCircleIcon />
+              <span className="ml-2 text-sm">Profil</span>
+            </div>
+          </Link>
+          <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md" onClick={togglePopupAvatar}>
+            <DoorIcon />
+            <span className="ml-2 text-sm">Keluar</span>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export { UserMenu };
