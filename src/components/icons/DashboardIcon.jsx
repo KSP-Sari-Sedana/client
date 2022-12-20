38ab678e49b2aed7b3f15acca044f4463354699e
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:28:18 +0800

    style: dashboard icon

---
+++ src/components/icons/DashboardIcon.jsx
@@ -0,0 +1,12 @@
+function DashboardIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="none" viewBox="0 0 20 18">
+      <path fill="#28303F" d="M0 10a2 2 0 012-2h16a2 2 0 012 2v4a4 4 0 01-4 4H4a4 4 0 01-4-4v-4z" opacity="0.4"></path>
+      <path fill="#28303F" d="M2 6a2 2 0 012-2h12a2 2 0 012 2v2H2V6z"></path>
+      <path fill="#28303F" d="M4 2a2 2 0 012-2h8a2 2 0 012 2v2H4V2z" opacity="0.4"></path>
+      <path fill="#28303F" fillRule="evenodd" d="M7.25 11a.75.75 0 01.75-.75h4a.75.75 0 010 1.5H8a.75.75 0 01-.75-.75z" clipRule="evenodd"></path>
+    </svg>
+  );
+}
+
+export { DashboardIcon };
