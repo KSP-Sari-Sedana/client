4cfa0c3ebdb20ce302218274f0410f618af8e163
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:03:00 +0800

    style: arrow right icon

---
+++ src/components/icons/ArrowRightIcon.jsx
@@ -0,0 +1,9 @@
+function ArrowRightIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" fill="none" viewBox="0 0 6 10" className="ml-2">
+      <path fill="#111827" fillRule="evenodd" d="M.293 9.707a1 1 0 010-1.414L3.586 5 .293 1.707A1 1 0 011.707.293l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+    </svg>
+  );
+}
+
+export { ArrowRightIcon };
