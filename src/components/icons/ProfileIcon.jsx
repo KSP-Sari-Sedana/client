06c893a39eddab3c816f31ac549d01f6a2cc4835
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 07:10:36 +0800

    style: profile icon

---
+++ src/components/icons/ProfileIcon.jsx
@@ -0,0 +1,10 @@
+function ProfileIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 14 18">
+      <circle cx="7" cy="4" r="4" fill="#28303F"></circle>
+      <ellipse cx="7" cy="14" fill="#28303F" opacity="0.4" rx="7" ry="4"></ellipse>
+    </svg>
+  );
+}
+
+export { ProfileIcon };
