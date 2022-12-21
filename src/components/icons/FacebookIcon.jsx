1ef6d21d46204e12293324a8d9776e32c8cc4e04
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-21 08:12:27 +0800

    style: facebook icon

---
+++ src/components/icons/FacebookIcon.jsx
@@ -0,0 +1,9 @@
+function FacebookIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="9" height="20" fill="none" viewBox="0 0 9 20">
+      <path fill="#3C5A99" d="M8.236 9.953H5.651v9.163h-3.83V9.953H0V6.714h1.82V4.62c0-1.5.718-3.844 3.866-3.844l2.839.01V3.93h-2.06c-.336 0-.813.167-.813.883v1.903h2.919l-.335 3.238z"></path>
+    </svg>
+  );
+}
+
+export { FacebookIcon };
