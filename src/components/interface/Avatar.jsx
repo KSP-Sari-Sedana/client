3fa3099a5c7f62dcf012b4b46d1e9097c4e81594
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-22 11:41:28 +0800

    style: avatar icon

--- src/components/interface/Avatar.jsx
+++ src/components/interface/Avatar.jsx
@@ -1,7 +1,9 @@
+import { AvatarIcon } from "../icons/AvatarIcon";
+
 function Avatar({ action }) {
   return (
     <div onClick={action} className={`flex justify-center overflow-hidden items-center w-12 h-12 bg-gray-300 rounded-[21px] ${action && "cursor-pointer"} `}>
-      <img src="assets/avatar.svg" alt="Profile Icon" />
+      <AvatarIcon />
     </div>
   );
 }
