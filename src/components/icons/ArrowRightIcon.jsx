5bfa3ff64943879754e2cc834db36a74883e44b6
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-23 20:58:59 +0800

    fix: arrow right icon

--- src/components/icons/ArrowRightIcon.jsx
+++ src/components/icons/ArrowRightIcon.jsx
@@ -1,7 +1,14 @@
 function ArrowRightIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" fill="none" viewBox="0 0 6 10" className="ml-2">
-      <path fill="#111827" fillRule="evenodd" d="M.293 9.707a1 1 0 010-1.414L3.586 5 .293 1.707A1 1 0 011.707.293l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 14 10" className="ml-2">
+      <path
+        fill="#28303F"
+        fillRule="evenodd"
+        d="M13.53 5.53a.75.75 0 000-1.06l-4-4a.75.75 0 00-1.06 1.06l2.72 2.72H1a.75.75 0 000 1.5h10.19L8.47 8.47a.75.75 0 001.06 1.06l4-4z"
+        clipRule="evenodd"
+        opacity="0.4"
+      ></path>
+      <path fill="#28303F" d="M13.53 4.47a.75.75 0 010 1.06l-4 4a.75.75 0 01-1.06-1.06l3.5-3.467-3.5-3.473A.75.75 0 019.53.47l4 4z"></path>
     </svg>
   );
 }
