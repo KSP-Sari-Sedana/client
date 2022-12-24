545b3fdb982c9ce91d322719aa987f29a5dc8e5c
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-24 09:25:54 +0800

    style: cheerful button

--- src/components/interface/Button.jsx
+++ src/components/interface/Button.jsx
@@ -4,21 +4,16 @@ import { Link } from "react-router-dom";
 import { ArrowRightIcon } from "../icons/ArrowRightIcon";
 import { SpinnerIcon } from "../icons/SpinnerIcon";
 
-function Button({ text, style, link, round, height, width, action, isLoading }) {
+function Button({ text, style, link, round, height, width, icon, action, isLoading }) {
   const electron = "bg-electron-500 hover:bg-electron-600 text-white text-sm text-center";
   const light = "bg-gray-100 hover:bg-gray-200 text-zinc-900 text-sm text-center";
   const bethlehem = "bg-bethlehem-700 hover:bg-bethlehem-800 text-white text-sm text-center";
-  const cheerful = "bg-cheerful-400 text-zinc-900 border-2 border-zinc-900 text-sm py-2 px-8 flex items-center";
-
-  let arrowRight = false;
+  const cheerful = "bg-cheerful-400 text-zinc-900 text-sm py-2 px-6";
 
   if (style === "electron") style = electron;
   else if (style === "light") style = light;
   else if (style === "bethlehem") style = bethlehem;
-  else if (style === "cheerful") {
-    style = cheerful;
-    arrowRight = true;
-  }
+  else if (style === "cheerful") style = cheerful;
 
   return (
     <Fragment>
@@ -34,7 +29,7 @@ function Button({ text, style, link, round, height, width, action, isLoading }) 
           <div className={`${style} ${height} ${width} ${round}`}>
             <div className="flex items-center justify-center">
               <button>{text}</button>
-              {arrowRight && <ArrowRightIcon />}
+              {icon === "arrowRight" && <ArrowRightIcon />}
             </div>
           </div>
         </Link>
