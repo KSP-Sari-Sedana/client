e0f478d157438a4a5f0d7368446cc7bf91a01031
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-24 09:42:09 +0800

    feat: login

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const authContext = createContext();
 
@@ -8,6 +9,12 @@ function useAuthContext() {
 
 function AuthProvider({ children }) {
   const [isAuth, setIsAuth] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState({ login: false });
+  const [APIResponse, setAPIResponse] = useState({});
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
@@ -15,7 +22,41 @@ function AuthProvider({ children }) {
     }
   }, []);
 
-  const authContextValue = { isAuth };
+  async function login() {
+    setIsLoading({ ...isLoading, login: true });
+    let result = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    result = await result.json();
+    setAPIResponse(result);
+    if (result.status !== "OK") {
+      setIsLoading({ ...isLoading, login: false });
+      return;
+    }
+    localStorage.setItem("token", result.data.token);
+    setIsAuth(true);
+    setIsLoading({ ...isLoading, login: false });
+    navigate("/dashboard");
+    window.location.reload();
+  }
+
+  const authContexts = {
+    isAuth,
+    email,
+    password,
+    APIResponse,
+    isLoading,
+    login,
+    setEmail,
+    setPassword,
+  };
+
+  const authContextValue = { authContexts };
 
   return <authContext.Provider value={authContextValue}>{children}</authContext.Provider>;
 }
