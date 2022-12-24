eee4223021c16a22af4fef6ebb01e2771884d984
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-24 09:24:16 +0800

    feat: button action and loading

--- src/components/interface/Button.jsx
+++ src/components/interface/Button.jsx
@@ -1,8 +1,10 @@
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 import { ArrowRightIcon } from "../icons/ArrowRightIcon";
+import { SpinnerIcon } from "../icons/SpinnerIcon";
 
-function Button({ text, style, link, round, height, width }) {
+function Button({ text, style, link, round, height, width, action, isLoading }) {
   const electron = "bg-electron-500 hover:bg-electron-600 text-white text-sm text-center";
   const light = "bg-gray-100 hover:bg-gray-200 text-zinc-900 text-sm text-center";
   const bethlehem = "bg-bethlehem-700 hover:bg-bethlehem-800 text-white text-sm text-center";
@@ -19,12 +21,25 @@ function Button({ text, style, link, round, height, width }) {
   }
 
   return (
-    <Link to={link}>
-      <div className={`${style} ${height} ${width} ${round}`}>
-        <button>{text}</button>
-        {arrowRight && <ArrowRightIcon />}
-      </div>
-    </Link>
+    <Fragment>
+      {isLoading ? (
+        <div className={`${style} ${height} ${width} ${round} opacity-70 cursor-not-allowed`}>
+          <div className="flex items-center justify-center">
+            <SpinnerIcon />
+            <button className="cursor-not-allowed">Loading</button>
+          </div>
+        </div>
+      ) : (
+        <Link to={link} onClick={action}>
+          <div className={`${style} ${height} ${width} ${round}`}>
+            <div className="flex items-center justify-center">
+              <button>{text}</button>
+              {arrowRight && <ArrowRightIcon />}
+            </div>
+          </div>
+        </Link>
+      )}
+    </Fragment>
   );
 }
 
