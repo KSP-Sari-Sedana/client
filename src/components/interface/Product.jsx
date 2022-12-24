f502d97289240b370df25f2323d362731e328293
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-24 10:17:50 +0800

    feat: product is action and link

--- src/components/interface/Product.jsx
+++ src/components/interface/Product.jsx
@@ -1,7 +1,7 @@
 import { Badge } from "./Badge";
 import { Button } from "./Button";
 
-function Product() {
+function Product({ isAction, link }) {
   return (
     <div className="flex flex-col items-center">
       <div className="w-80 bg-white border border-slate-200 rounded-xl p-4 shadow-sm">
@@ -11,14 +11,18 @@ function Product() {
           <Badge style="clear" content="Simpanan" />
           <Badge style="clear" content="0,5%" />
           <p className="text-sm mt-2 text-slate-600">Simpanan program yang diperuntukan untuk kegiatan upacara keagamaan.</p>
-          <div className="flex items-center justify-between mt-2">
-            <div className="flex -space-x-4">
-              <img className="w-10 h-10 rounded-full border-4 border-white" src="/assets/profile-picture-bagus.svg" alt="" />
-              <img className="w-10 h-10 rounded-full border-4 border-white" src="/assets/profile-picture-rama.svg" alt="" />
-              <img className="w-10 h-10 rounded-full border-4 border-white" src="/assets/profile-picture-rizky.svg" alt="" />
+          <div className="flex items-center mt-2">
+            <div>
+              <div className="flex -space-x-4">
+                <img className="w-10 h-10 rounded-full border-4 border-white" src="/assets/profile-picture-bagus.svg" alt="" />
+                <img className="w-10 h-10 rounded-full border-4 border-white" src="/assets/profile-picture-rama.svg" alt="" />
+                <img className="w-10 h-10 rounded-full border-4 border-white" src="/assets/profile-picture-rizky.svg" alt="" />
+              </div>
             </div>
-            <p className="flex justify-center items-center mr-5 w-10 h-10 text-sm font-sourcecodepro font-bold bg-transparent">+154</p>
-            <Button text="Ajukan" style="cheerful" round="rounded-md" />
+            <div>
+              <p className="flex justify-center items-center w-10 h-10 text-sm font-sourcecodepro font-bold bg-transparent">+154</p>
+            </div>
+            <div className="ml-5">{isAction && <Button icon="arrowRight" link={link} text="Ajukan" style="cheerful" round="rounded-md" />}</div>
           </div>
         </div>
       </div>
