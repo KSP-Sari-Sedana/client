721b97743aedb2e3e659202b86dda1cc96bbb97e
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-24 09:01:20 +0800

    style: spinner icon

---
+++ src/components/icons/SpinnerIcon.jsx
@@ -0,0 +1,10 @@
+function SpinnerIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 56 56" className="animate-spin mr-2">
+      <path opacity="0.4" stroke="#D9D9D9" strokeWidth="8" d="M36.639 5.609a24 24 0 11-17.277 44.783A24 24 0 0136.639 5.609h0z"></path>
+      <path opacity="0.9" stroke="#2D2D2D" strokeLinecap="round" strokeWidth="8" d="M36.639 5.609a24 24 0 0115.326 23.688"></path>
+    </svg>
+  );
+}
+
+export { SpinnerIcon };
