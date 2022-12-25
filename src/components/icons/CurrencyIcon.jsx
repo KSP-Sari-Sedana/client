0eee3a47e0dd19ed57cf1fcdff067403163876ac
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-25 10:18:34 +0800

    style: currency icon

---
+++ src/components/icons/CurrencyIcon.jsx
@@ -0,0 +1,15 @@
+function CurrencyIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
+      <path
+        stroke="#ACB4C3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 6c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8v8m0-8c1.11 0 2.08.402 2.599 1M10 6V5m0 9v1m0-1c-1.11 0-2.08-.402-2.599-1M19 10a9 9 0 11-18 0 9 9 0 0118 0z"
+      ></path>
+    </svg>
+  );
+}
+
+export { CurrencyIcon };
