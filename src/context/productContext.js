c1c647fb3358af346bdcb84262a330a1b2c4734c
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-25 10:35:54 +0800

    feat: get product by id

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -7,6 +7,7 @@ function useProductContext() {
 }
 
 function ProductProvider({ children }) {
+  const [product, setProduct] = useState({});
   const [products, setProducts] = useState([]);
 
   async function getProducts() {
@@ -18,9 +19,20 @@ function ProductProvider({ children }) {
     setProducts(result.data.products);
   }
 
+  async function getProductById(id) {
+    let result = await fetch(`${process.env.REACT_APP_API_URL}/api/products/${id}`);
+    result = await result.json();
+    if (result.status !== "OK") {
+      return;
+    }
+    setProduct(result.data.product);
+  }
+
   const productContexts = {
+    product,
     products,
     getProducts,
+    getProductById,
   };
 
   const productContextValue = { productContexts };
