f0170d6123a1ff108658eb5b11855c590bae8198
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-27 11:26:14 +0800

    fix(api): product result

--- src/components/interface/Product.jsx
+++ src/components/interface/Product.jsx
@@ -9,10 +9,10 @@ function Product(props) {
       <div className="w-80 bg-white border border-slate-200 rounded-xl p-4 shadow-sm">
         <img src="/assets/placeholder.svg" alt="" className="mx-auto" />
         <div className="px-2">
-          <p className="font-sourcecodepro font-bold text-2xl mt-2 mb-2">{product.nama}</p>
-          <Badge style="clear" content={product.tipe} />
-          <Badge style="clear" content={`${product.bunga}%`} />
-          <p className={`text-sm mt-2 text-slate-600 font-normal ${isPreview && "h-11 overflow-hidden"}`}>{product.deskripsi}</p>
+          <p className="font-sourcecodepro font-bold text-2xl mt-2 mb-2">{product.name}</p>
+          <Badge style="clear" content={product.type} />
+          <Badge style="clear" content={`${product.interest}%`} />
+          <p className={`text-sm mt-2 text-slate-600 font-normal ${isPreview && "h-11 overflow-hidden"}`}>{product.description}</p>
           <div className="flex items-center mt-2">
             <div>
               <div className="flex -space-x-4">
