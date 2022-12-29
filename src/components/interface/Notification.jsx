ba3b8a01099ccfaccd7a65a1752608ac42f36ff4
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-29 13:31:54 +0800

    fix: popup transition

--- src/components/interface/Notification.jsx
+++ src/components/interface/Notification.jsx
@@ -1,27 +1,39 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+
 import { Announcement } from "./Announcement";
 import { TollerIcon } from "../icons/TollerIcon";
-import { useToggleContext } from "../../context/toggleContext";
 
 function Notification() {
-  const { isPopupNotif, togglePopupNotif } = useToggleContext();
-
   return (
-    <>
-      <div className="cursor-pointer" onClick={togglePopupNotif}>
-        <TollerIcon />
-        <div className="absolute h-3 w-3 bg-bethlehem-700 top-[30px] ml-2 rounded-full border-2 border-white"></div>
-      </div>
-      {isPopupNotif && (
-        <div className="w-96 h-96 overflow-auto rounded-2xl bg-white border border-slate-200 shadow-sm px-3 py-3 absolute top-[67px] right-0">
-          <p className="text-sm text-center">Notifikasi 10 hari terakhir:</p>
-          <div>
-            <Announcement category="Transaksi" detail="Transaksi sebesar Rp.300.000 dilakukan pada produk SIPURA" isRead={true} />
-            <Announcement category="Pengajuan" detail="Selamat pengajuan produk SIPURA anda diterima" isRead={false} />
-            <Announcement category="Akun" detail="Selamat datang, lengkapi profil untuk memulai menikmati produk" isRead={false} />
-          </div>
-        </div>
-      )}
-    </>
+    <Fragment>
+      <Popover>
+        <Popover.Button className="flex focus:outline-none">
+          <TollerIcon />
+          <div className="absolute h-3 w-3 bg-bethlehem-700 top-[30px] ml-2 rounded-full border-2 border-white"></div>
+        </Popover.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
+        >
+          <Popover.Panel>
+            <div className="w-96 h-96 overflow-auto rounded-2xl bg-white border border-slate-200 shadow-sm px-3 py-3 absolute top-[8px] -right-5">
+              <p className="text-sm text-center">Notifikasi 10 hari terakhir:</p>
+              <div>
+                <Announcement category="Transaksi" detail="Transaksi sebesar Rp.300.000 dilakukan pada produk SIPURA" isRead={true} />
+                <Announcement category="Pengajuan" detail="Selamat pengajuan produk SIPURA anda diterima" isRead={false} />
+                <Announcement category="Akun" detail="Selamat datang, lengkapi profil untuk memulai menikmati produk" isRead={false} />
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    </Fragment>
   );
 }
 
--- src/components/interface/UserMenu.jsx
+++ src/components/interface/UserMenu.jsx
@@ -1,58 +1,71 @@
 import { Link } from "react-router-dom";
 import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 
 import { Avatar } from "./Avatar";
 import { GoldStarIcon } from "../icons/GoldStarIcon";
 import { DashboardIcon } from "../icons/DashboardIcon";
 import { ProfileCircleIcon } from "../icons/ProfileCircleIcon";
 import { DoorIcon } from "../icons/DoorIcon";
-import { useToggleContext } from "../../context/toggleContext";
 import { useAuthContext } from "../../context/authContext";
 
 function UserMenu() {
-  const { isPopupAvatar, togglePopupAvatar } = useToggleContext();
   const { authContexts } = useAuthContext();
+  const { logout } = authContexts;
 
   return (
     <Fragment>
-      <Avatar action={togglePopupAvatar} />
-      {isPopupAvatar && (
-        <div className="w-60 rounded-2xl bg-white border border-slate-200 shadow-sm px-3 py-3 absolute top-[67px] right-0">
-          <div className="flex items-center">
-            <Avatar />
-            <div className="ml-3">
-              <p className="font-darkergrotesque font-bold text-2xl leading-none">Suprapta</p>
+      <Popover>
+        <Popover.Button className="flex focus:outline-none">
+          <Avatar />
+        </Popover.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
+        >
+          <Popover.Panel>
+            <div className="w-60 rounded-2xl bg-white border border-slate-200 shadow-sm px-3 py-3 absolute top-[8px] -right-5">
               <div className="flex items-center">
-                <GoldStarIcon />
-                <p className="text-sm text-slate-500 leading-tight">Admin</p>
+                <Avatar />
+                <div className="ml-3">
+                  <p className="font-darkergrotesque font-bold text-2xl leading-none">Suprapta</p>
+                  <div className="flex items-center">
+                    <GoldStarIcon />
+                    <p className="text-sm text-slate-500 leading-tight">Admin</p>
+                  </div>
+                </div>
+              </div>
+              <hr className="my-3 h-px bg-gray-200 border-0"></hr>
+              <Link to="/dashboard">
+                <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md">
+                  <DashboardIcon />
+                  <span className="ml-2 text-sm">Dashboard</span>
+                </div>
+              </Link>
+              <Link to="/profile">
+                <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md">
+                  <ProfileCircleIcon />
+                  <span className="ml-2 text-sm">Profil</span>
+                </div>
+              </Link>
+              <div
+                className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md"
+                onClick={() => {
+                  logout();
+                }}
+              >
+                <DoorIcon />
+                <span className="ml-2 text-sm">Keluar</span>
               </div>
             </div>
-          </div>
-          <hr className="my-3 h-px bg-gray-200 border-0"></hr>
-          <Link to="/dashboard">
-            <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md" onClick={togglePopupAvatar}>
-              <DashboardIcon />
-              <span className="ml-2 text-sm">Dashboard</span>
-            </div>
-          </Link>
-          <Link to="/profile">
-            <div className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md" onClick={togglePopupAvatar}>
-              <ProfileCircleIcon />
-              <span className="ml-2 text-sm">Profil</span>
-            </div>
-          </Link>
-          <div
-            className="inline-flex items-center cursor-pointer w-full hover:bg-zinc-100 p-2 rounded-md"
-            onClick={() => {
-              togglePopupAvatar();
-              authContexts.logout();
-            }}
-          >
-            <DoorIcon />
-            <span className="ml-2 text-sm">Keluar</span>
-          </div>
-        </div>
-      )}
+          </Popover.Panel>
+        </Transition>
+      </Popover>
     </Fragment>
   );
 }
