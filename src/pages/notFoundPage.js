db1b51ced56cf7943c0896118bc8134468cbc0f1
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-29 12:41:12 +0800

    style: centered not found page

--- src/pages/notFoundPage.js
+++ src/pages/notFoundPage.js
@@ -4,10 +4,12 @@ function NotFoundPage() {
   return (
     <div>
       <Navbar />
-      <div className="mt-32">
-        <img src="/assets/placeholder.svg" alt="" className="mx-auto w-48" />
-        <p className="font-darkergrotesque text-center text-9xl font-bold">404</p>
-        <p className="text-center text-slate-600">Halaman tidak ditemukan</p>
+      <div className="flex items-center justify-center h-screen">
+        <div>
+          <img src="/assets/placeholder.svg" alt="" className="mx-auto w-48 font" />
+          <p className="font-darkergrotesque text-center text-9xl font-bold">404</p>
+          <p className="text-center text-slate-600">Halaman tidak ditemukan</p>
+        </div>
       </div>
     </div>
   );
