e020c3620a279ffb91c19afbf71eae71e0f7ec67
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-30 06:44:24 +0800

    style: current color arrow icon

--- src/components/icons/ArrowIcon.jsx
+++ src/components/icons/ArrowIcon.jsx
@@ -1,15 +1,19 @@
 function ArrowIcon({ aim }) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 14 10" className="ml-2">
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 10" className="ml-2">
       <path
-        fill="#28303F"
+        fill="currentColor"
         fillRule="evenodd"
         transform={`${aim === "rightTop" && "rotate(-45 7 5)"}`}
         d="M13.53 5.53a.75.75 0 000-1.06l-4-4a.75.75 0 00-1.06 1.06l2.72 2.72H1a.75.75 0 000 1.5h10.19L8.47 8.47a.75.75 0 001.06 1.06l4-4z"
         clipRule="evenodd"
         opacity="0.4"
       ></path>
-      <path transform={`${aim === "rightTop" && "rotate(-45 7 5)"}`} fill="#28303F" d="M13.53 4.47a.75.75 0 010 1.06l-4 4a.75.75 0 01-1.06-1.06l3.5-3.467-3.5-3.473A.75.75 0 019.53.47l4 4z"></path>
+      <path
+        transform={`${aim === "rightTop" && "rotate(-45 7 5)"}`}
+        fill="currentColor"
+        d="M13.53 4.47a.75.75 0 010 1.06l-4 4a.75.75 0 01-1.06-1.06l3.5-3.467-3.5-3.473A.75.75 0 019.53.47l4 4z"
+      ></path>
     </svg>
   );
 }
