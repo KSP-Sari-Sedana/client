72052e25039b80a6b367af2418a0319f0d4cf888
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-29 11:07:20 +0800

    style: location font

--- src/components/interface/Location.jsx
+++ src/components/interface/Location.jsx
@@ -4,21 +4,21 @@ function Location() {
       <div className="grid grid-cols-3 mx-20">
         <div>
           <img src="/assets/placeholder.svg" alt="" className="mx-auto w-52" />
-          <p className="font-sourcecodepro text-xl font-bold text-center mt-3">KCP RENDANG</p>
+          <p className="text-xl font-bold text-center mt-3">Cabang Rendang</p>
           <p className="text-center text-sm text-slate-600">
             Jl. Gunung Guning No. 69, Singarata <br /> Rendang, Karangasem, Bali 80863
           </p>
         </div>
         <div>
           <img src="/assets/placeholder.svg" alt="" className="mx-auto w-60" />
-          <p className="font-sourcecodepro text-2xl font-bold text-center mt-3">KANTOR PUSAT SELAT</p>
+          <p className="text-2xl font-bold text-center mt-3">Kantor Pusat Selat</p>
           <p className="text-center text-slate-600">
             Pering Sari, Kec. Selat, Kabupaten <br /> Karangasem, Bali 80862
           </p>
         </div>
         <div>
           <img src="/assets/placeholder.svg" alt="" className="mx-auto w-52" />
-          <p className="font-sourcecodepro text-xl font-bold text-center mt-3">KCP KARANGASEM</p>
+          <p className="text-xl font-bold text-center mt-3">Cabang Karangasem</p>
           <p className="text-center text-sm text-slate-600">
             Jl. Veteran, Padang Kerta, Karangasem, <br />
             Bali 80811
