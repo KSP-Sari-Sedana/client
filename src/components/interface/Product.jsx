1e13d3aeaa91b53013d6145dc3f11a96cc126b2a
gusde <69630915+slickeel@users.noreply.github.com>
2022-12-29 13:52:50 +0800

    style: display deposit in product

--- src/components/interface/Product.jsx
+++ src/components/interface/Product.jsx
@@ -10,8 +10,11 @@ function Product(props) {
         <img src="/assets/placeholder.svg" alt="" className="mx-auto" />
         <div className="px-2">
           <p className="font-sourcecodepro font-bold text-2xl mt-2 mb-2">{product.name}</p>
-          <Badge style="clear" content={product.type} />
-          <Badge style="clear" content={`${product.interest}%`} />
+          <div className="flex">
+            <Badge style="clear" content={product.type} />
+            <Badge style="clear" content={`${product.interest}%`} />
+            <Badge style="clear" content={`${product.deposit}`} />
+          </div>
           <p className={`text-sm mt-2 text-slate-600 font-normal ${isPreview && "h-11 overflow-hidden"}`}>{product.description}</p>
           <div className="flex items-center mt-2">
             <div>
@@ -24,7 +27,7 @@ function Product(props) {
             <div className="grow">
               <p className="text-sm font-sourcecodepro font-bold">+154</p>
             </div>
-            <div>{isPreview && <Button icon="arrowRight" link={`/products/${product.id}`} text="Ajukan" style="cheerful" round="rounded-md" />}</div>
+            <div>{isPreview && <Button icon="arrow" link={`/products/${product.id}`} text="Ajukan" style="cheerful" round="rounded-md" />}</div>
           </div>
         </div>
       </div>
