36d6ceab256ae33eb035ef1eeb4bd79e15a07c15
gusde <69630915+slickeel@users.noreply.github.com>
2023-01-02 17:53:04 +0800

    style: saving card

---
+++ src/components/icons/ChipIcon.jsx
@@ -0,0 +1,12 @@
+function ChipIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="26" fill="none" viewBox="0 0 35 26">
+      <g stroke="#ACB4C3" strokeWidth="1.4" opacity="0.8">
+        <rect width="33.6" height="24.6" x="0.7" y="0.7" fill="#fff" fillOpacity="0.26" rx="4.3"></rect>
+        <path d="M.547 7.998h7.86a3.625 3.625 0 013.624 3.625v2.64m0 11.224v-4.959m0 0v-6.265m0 6.265H.547m11.484-6.265H.547M34.453 7.998H23.516m0 0v6.265m0-6.265V.533m0 24.954V20.53m0 0v-6.266m0 6.266h10.937m-10.937-6.266h10.937"></path>
+      </g>
+    </svg>
+  );
+}
+
+export { ChipIcon };
---
+++ src/components/interface/SavingCard.jsx
@@ -0,0 +1,36 @@
+import { ChipIcon } from "../icons/ChipIcon";
+import { SwellArt } from "../art/SwellArt";
+import { useUserContext } from "../../context/userContext";
+
+function SavingCard() {
+  const { userContexts } = useUserContext();
+  const { me } = userContexts;
+
+  return (
+    <div className="w-80 h-52 rounded-2xl border flex shadow-sm bg-white overflow-hidden relative">
+      <div className="absolute left-56 bottom-4">
+        <SwellArt />
+      </div>
+      <div className="absolute h-full p-5">
+        <div className="flex flex-col justify-between h-full">
+          <div className="grow">
+            <ChipIcon />
+          </div>
+          <div className="mb-2 font-darkergrotesque text-2xl font-extrabold">
+            <div className="flex gap-x-2">
+              <p>1253</p>
+              <p>5432</p>
+            </div>
+            <div className="flex gap-x-2">
+              <p>3521</p>
+              <p>3090</p>
+            </div>
+          </div>
+          <p className="font-sourcecodepro font-medium tracking-tighter">{`${me.firstName} ${me.lastName}`}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { SavingCard };
