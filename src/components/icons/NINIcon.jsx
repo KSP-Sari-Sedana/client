2bd6a8c810870556fe0790b295eb99bf27f5817f
gusde <69630915+slickeel@users.noreply.github.com>
2023-01-02 18:03:04 +0800

    style: input icon and disable

---
+++ src/components/icons/NINIcon.jsx
@@ -0,0 +1,15 @@
+function NINIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="none" viewBox="0 0 20 18">
+      <path
+        stroke="#ACB4C3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M8 4H3a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V6a2 2 0 00-2-2h-5M8 4V3a2 2 0 114 0v1M8 4a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M7 12a3.001 3.001 0 00-2.83 2M13 9h3m-3 4h2"
+      ></path>
+    </svg>
+  );
+}
+
+export { NINIcon };
--- src/components/interface/Input.jsx
+++ src/components/interface/Input.jsx
@@ -2,8 +2,10 @@ import { AtIcon } from "../icons/AtIcon";
 import { KeyIcon } from "../icons/KeyIcon";
 import { FingerPrintIcon } from "../icons/FingerPrintIcon";
 import { CurrencyIcon } from "../icons/CurrencyIcon";
+import { NINIcon } from "../icons/NINIcon";
+import { BriefcaseIcon } from "../icons/BriefcaseIcon";
 
-function Input({ label, type, placeHolder, icon, value, action }) {
+function Input({ label, type, placeHolder, icon, value, action, isDisabled }) {
   return (
     <div className="mb-4">
       <label className="block mb-2 text-sm">{label}</label>
@@ -14,16 +16,19 @@ function Input({ label, type, placeHolder, icon, value, action }) {
             {icon === "password" && <KeyIcon />}
             {icon === "fingerPrint" && <FingerPrintIcon />}
             {icon === "currency" && <CurrencyIcon />}
+            {icon === "nin" && <NINIcon />}
+            {icon === "job" && <BriefcaseIcon />}
           </div>
         )}
         <input
           onChange={(event) => {
             action(event.target.value);
           }}
+          disabled={isDisabled}
           type={type}
           className={`border border-gray-300 text-sm text-zinc-900 rounded-lg w-full p-2.5 ${icon ? "pl-10" : "pl-4"} focus:outline-none`}
           placeholder={placeHolder}
-          defaultValue={value}
+          value={value}
         />
       </div>
     </div>
