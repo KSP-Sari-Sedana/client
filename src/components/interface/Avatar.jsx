58619aff52ce850c182c166e815a95e25e7dce76
gusde <69630915+slickeel@users.noreply.github.com>
2023-01-02 16:15:38 +0800

    fix: avatar src and logic

--- src/components/interface/Avatar.jsx
+++ src/components/interface/Avatar.jsx
@@ -9,11 +9,9 @@ function Avatar() {
     getMyProfile();
   }, []);
 
-  console.log(me.image);
-
   return (
     <div className={`flex justify-center overflow-hidden items-center w-12 h-12 bg-zinc-300 rounded-[21px]`}>
-      {me.image === null || me.image === undefined ? <img src={me.image} alt="Avatar" /> : <img src="https://source.boringavatars.com/beam/120?square" alt="Avatar" />}
+      {me.image === null || me.image === undefined ? <img src="https://source.boringavatars.com/pixel/120?square" alt="Avatar" /> : <img src={me.image} alt="Avatar" />}
     </div>
   );
 }
