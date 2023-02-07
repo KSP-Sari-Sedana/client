0089e91173b83f32869a3f1bcd47cac89a803ca6
gusde <69630915+slickeel@users.noreply.github.com>
2023-02-07 08:37:08 +0800

    style: not found page art

--- src/pages/notFoundPage.js
+++ src/pages/notFoundPage.js
@@ -1,4 +1,5 @@
 import { Navbar } from "../components/interface/Navbar";
+import { GirlSadArt } from "../components/art/GirlSadArt";
 
 function NotFoundPage() {
   return (
@@ -6,9 +7,9 @@ function NotFoundPage() {
       <Navbar />
       <div className="flex items-center justify-center h-screen">
         <div>
-          <img src="/assets/placeholder.svg" alt="" className="mx-auto w-48 font" />
-          <p className="font-darkergrotesque text-center text-9xl font-bold">404</p>
-          <p className="text-center text-slate-600">Halaman tidak ditemukan</p>
+          <GirlSadArt />
+          <p className="font-darkergrotesque text-center text-8xl font-bold">404</p>
+          <p className="text-center text-slate-600 text-sm">Halaman tidak ditemukan</p>
         </div>
       </div>
     </div>
