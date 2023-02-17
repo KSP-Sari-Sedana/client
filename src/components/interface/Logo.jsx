2a0c7781ee885fc44bbfd36469cf8fe085d9af8f
gusde <69630915+slickeel@users.noreply.github.com>
2023-02-17 08:55:15 +0800

    style: logo

--- src/components/interface/Logo.jsx
+++ src/components/interface/Logo.jsx
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
-import { LogoIcon } from "../icons/LogoIcon";
 
 function Logo() {
   return (
     <Link to="/">
       <div className="flex items-center">
-        <LogoIcon />
-        <p className="text-3xl tracking-tighter ml-2">
+        <p className="text-3xl tracking-tighter">
           <span className="font-extrabold">Sari</span>
           <span className="font-normal">Sedana</span>
         </p>
