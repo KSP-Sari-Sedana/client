f266eee1c1e8866b989e02ebccf3b59e347c0a2e
gusde <69630915+slickeel@users.noreply.github.com>
2023-02-19 23:10:32 +0800

    fix: heading margin

--- src/components/interface/Heading.jsx
+++ src/components/interface/Heading.jsx
@@ -1,6 +1,6 @@
 function Heading({ text, id }) {
   return (
-    <h1 id={id} className={`font-darkergrotesque text-center text-6xl font-bold mt-32 mb-8`}>
+    <h1 id={id} className={`font-darkergrotesque text-center text-6xl font-bold mt-16 mb-8`}>
       {text}
     </h1>
   );
