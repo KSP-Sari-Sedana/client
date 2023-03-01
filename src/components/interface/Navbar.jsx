c1f6908703c8976649ff4a8d2d724ba7acdee0ad
gusde <69630915+slickeel@users.noreply.github.com>
2023-03-01 14:45:53 +0800

    style: navbar

--- src/components/interface/Navbar.jsx
+++ src/components/interface/Navbar.jsx
@@ -8,26 +8,28 @@ function Navbar() {
   const { authCtx } = useAuthContext();
 
   return (
-    <div className="bg-pampas-50 px-2 py-2.5 fixed w-10/12 top-0 border-b border-slate-200 z-10 min-w-min">
-      <div className="flex items-center justify-between">
-        <div className="w-72">
+    <div className="bg-pampas-50 px-2 py-2.5 fixed w-10/12 top-0 border-b border-slate-200 z-10">
+      <div className="items-center justify-between flex">
+        <div className="w-48">
           <Logo />
         </div>
-        <nav className="flex grow px-36 p-4 space-x-14 justify-between text-zinc-500 text-sm">
-          <a href="/#product" className="hover:text-zinc-800">
-            Produk
-          </a>
-          <a href="/#location" className="hover:text-zinc-800">
-            Lokasi
-          </a>
-          <a href="/#about" className="hover:text-zinc-800">
-            Tentang
-          </a>
-          <a href="/#footer" className="hover:text-zinc-800">
-            Kontak
-          </a>
-        </nav>
-        <div className="w-72">
+        <div>
+          <nav className="flex p-4 space-x-20 justify-between text-zinc-500 text-sm">
+            <a href="/#product" className="hover:text-zinc-800">
+              Produk
+            </a>
+            <a href="/#location" className="hover:text-zinc-800">
+              Lokasi
+            </a>
+            <a href="/#about" className="hover:text-zinc-800">
+              Tentang
+            </a>
+            <a href="/#footer" className="hover:text-zinc-800">
+              Kontak
+            </a>
+          </nav>
+        </div>
+        <div className="w-48">
           {authCtx.isLoggedIn ? (
             <div className="flex place-content-end">
               <div className="flex items-center space-x-8">
